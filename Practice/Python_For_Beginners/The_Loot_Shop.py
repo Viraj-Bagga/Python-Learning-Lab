74a86a196d73cd2ce822341c92326d05d91711f0
Viraj Bagga <90282313+Viraj-Bagga@users.noreply.github.com>
2026-05-30 15:22:26 -0400

    Creation and organiztion of the Python Learning Lab. Also starting to work on the first project, The Loot Shop.

---
+++ Practice/Python_For_Beginners/The_Loot_Shop.py
@@ -0,0 +1,30 @@
+# The Loot Shop
+
+# Function : The player starts with 100 gold coins. Players can open 10 gold coin loot boxes until they choose to stop, or run out of money. If the user pulls a legendary item, they immeditally recieve 20 gold coins. 
+
+loot_box = {
+    "Sword" : {
+        "Rarity" : 2,
+        "Cost" : 5
+    },
+    "Shield" : {
+        "Rarity" : 3,
+        "Cost" : 5
+    },
+    "Invisability Potion" : {
+        "Rarity" : 4,
+        "Cost" : 5
+    },
+    "Rusty Blade" : {
+        "Rarity" : 6,
+        "Cost" : 5
+    },
+    "Armour" : {
+        "Rarity" : 7,
+        "Cost" : 5
+    },
+    "Magic Wand" : {
+        "Rarity" : 9,
+        "Cost" : 5
+    }
+}
