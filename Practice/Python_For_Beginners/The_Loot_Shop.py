9d6c5f33bee48000b788e872c002beb60a778a93
Viraj Bagga <90282313+Viraj-Bagga@users.noreply.github.com>
2026-05-30 18:39:10 -0400

    .

--- Practice/Python_For_Beginners/The_Loot_Shop.py
+++ Practice/Python_For_Beginners/The_Loot_Shop.py
@@ -3,42 +3,50 @@
 # Function : The player starts with 100 gold coins. Players can open 10 gold coin loot boxes until they choose to stop, or run out of money. If the user pulls a legendary item, they immeditally recieve 20 gold coins. 
 
 import random
+import numpy as np
 
 loot_box = {
     "Sword" : {
         "Rarity" : 2,
         "Cost" : 5,
-        "Chance" : .1
+        "Chance" : .2
     },
     "Shield" : {
         "Rarity" : 3,
         "Cost" : 5,
-        "Chance" : .2
+        "Chance" : .4
     },
     "Invisability Potion" : {
         "Rarity" : 4,
         "Cost" : 5,
-        "Chance" : .2
+        "Chance" : .6
     },
     "Rusty Blade" : {
         "Rarity" : 6,
         "Cost" : 5,
-        "Chance" : .2
+        "Chance" : .8
     },
     "Armour" : {
         "Rarity" : 7,
         "Cost" : 5,
-        "Chance" : .1
+        "Chance" : .9
     },
     "Magic Wand" : {
         "Rarity" : 9,
         "Cost" : 5,
-        "Chance" : .1
+        "Chance" : 1.0
     }
 }
 
+#Numpy cant pick random value with weights on a dested dictionary so I need to flaten it first
+loot = ["Sword", "Shield", "Invisability Potion", "Rusty Blade", "Armour", "Magic Wand"]
+
+#Keeps track of the players coin count at any given moment
 current_coins = 100
 
+items = []
+
+weights = [.2,.2,.2,.2,.1,.1]
 
 #User starts with 100 gold coins
 
@@ -47,6 +55,7 @@ def main():
     print("Welcome to my mystical loot shop. It appears so thought you dont have any money? That is ok, I will give you 100 gold coins to do with your desire. Each box cost 10 coins, so be wise with your decisions!")
 
     global current_coins
+    global items
 
     while current_coins != 0:
         currentChoice = makeChoice()
@@ -57,10 +66,7 @@ def main():
         else :
             break
 
-    print("That loop was broken")
-
-    
-
+    finalMessage()
 
 
 def makeChoice():
@@ -70,18 +76,32 @@ def makeChoice():
 
 def openLoot():
 
-    print("Opening Loot")
+    
     global loot_box
     global current_coins
-    item = random.choice(list(loot_box))
-    print(item)
+    global items
+    global weights
 
-    if (loot_box.get(item,{}).get("Rarity",{}) >= 8):
+    item = np.random.choice(list(loot_box), size = 1, p=weights)[0]
+
+    print(f"You recieved a {item}")
+    items.append(item)
+    
+    if loot_box[item+""].get("Rarity",0) >= 8:
         current_coins += 20
         print("Congrats! You recieved 20 more coins!")
 
 
+    
+
+
 def finalMessage():
-    print("Final Message")
+    
+    global items
+    global current_coins
+
+    totalItems = len(items)
+
+    print(f"You recieved {totalItems} and have {current_coins} gold coins remaining!")
 
 main()
